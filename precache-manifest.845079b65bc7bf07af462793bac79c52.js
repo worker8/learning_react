ae74f27e8a00abbe666eaee79ffd3b39557966dd
Tan Jun Rong <tanjunrong@gmail.com>
2019-11-21 15:54:50 +0900

    Updates

---
+++ precache-manifest.845079b65bc7bf07af462793bac79c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a2d7b2b5a5a7ce885f88be4685fb75",
+    "url": "/learning_react/index.html"
+  },
+  {
+    "revision": "11fb8699c67c81c29268",
+    "url": "/learning_react/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "09e40d25a8aa2acb8d79",
+    "url": "/learning_react/static/css/main.84aa1dea.chunk.css"
+  },
+  {
+    "revision": "11fb8699c67c81c29268",
+    "url": "/learning_react/static/js/2.d989a8a4.chunk.js"
+  },
+  {
+    "revision": "09e40d25a8aa2acb8d79",
+    "url": "/learning_react/static/js/main.f57cc972.chunk.js"
+  },
+  {
+    "revision": "446bcb9aaa0191b184fd",
+    "url": "/learning_react/static/js/runtime-main.4938e142.js"
+  }
+]);
